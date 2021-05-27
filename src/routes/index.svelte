20dfa9d413d6de162cb4f5b6aca60c2b1bab20bf
Conduitry <git@chor.date>
2021-05-27 17:28:55 -0400

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { onMount } from 'svelte';
+
+	if (false) {
+		// UNCOMMENT THE FOLLOWING LINE TO TRIGGER THE BUG
+		// import('@sentry/node');
+	}
+
+	onMount(async () => {
+		console.log(await import('decoders'));
+	})
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
